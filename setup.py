cd0013efa858176fbf010803ebb10e1861796fe0
BalduinLandolt <balduin.landolt@hotmail.com>
2021-01-25 15:48:27 +0100

    Bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='dsp-metadata-gui',
-    version='0.3.6',
+    version='0.3.7',
     description='Python GUI tool to collect metadata for DSP projects.',
     long_description=long_description,
     long_description_content_type="text/markdown",
