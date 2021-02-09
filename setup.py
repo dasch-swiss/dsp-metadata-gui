a80104ea8683f1b739af24948a0f6411635fd24f
BalduinLandolt <balduin.landolt@hotmail.com>
2021-02-09 17:55:35 +0100

    chore: bump version to rc4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dsp-metadata-gui",
-    version="0.0.0-rc3",
+    version="0.0.0-rc4",
     description="Python GUI tool to collect metadata for DSP projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
