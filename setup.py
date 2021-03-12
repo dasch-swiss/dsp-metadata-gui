b5684587a0058a4dfc71a2840b17018b28f8b863
BalduinLandolt <balduin.landolt@hotmail.com>
2021-03-12 11:27:48 +0100

    chore: bump version to 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dsp-metadata-gui",
-    version="0.0.0-rc7",
+    version="1.0.0",
     description="Python GUI tool to collect metadata for DSP projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
