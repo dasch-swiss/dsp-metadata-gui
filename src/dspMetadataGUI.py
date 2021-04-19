736c5fb1c921c8b5609c3420eab37b7326cab0ab
Balduin Landolt <balduin.landolt@hotmail.com>
2021-04-19 21:26:34 +0200

    feat: add tkInter test for pipenv

---
+++ src/dspMetadataGUI.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+
+
+# from tkinter import Tk
+
+
+# def main():
+#     root = Tk()
+    
+#     root.mainloop()
+
+
+# if __name__ == "__main__":
+#     main()
