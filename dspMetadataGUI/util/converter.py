aeaada879fb5ed6950ebf8c5a1d8bd8eea12f8a4
Balduin Landolt <balduin.landolt@hotmail.com>
2021-05-07 19:30:08 +0200

    feat: finish dataset

--- dspMetadataGUI/util/converter.py
+++ dspMetadataGUI/util/converter.py
@@ -1,5 +1,6 @@
+from typing import List
 from rdflib import Graph
-from rdflib.namespace import Namespace, RDF, SDO
+from rdflib.namespace import Namespace, RDF, SDO, PROV
 import json
 import jsonschema
 from rdflib.term import BNode
@@ -24,9 +25,10 @@ def convert_string(data):
     res = {"$schema": schema_url}
     res['project'] = _get_project(g)
     res['datasets'] = _get_datasets(g)
-    # TODO: datasets need to be added to project too
-    print(json.dumps(res, indent=2))
-    # validate(res)  # TODO: bring back
+    for ds in res.get('datasets'):
+        res['project']['datasets'].append(ds.get('@id'))
+    # print(json.dumps(res, indent=2))
+    validate(res)  # TODO: bring back
 
 
 # Dataset
@@ -41,22 +43,68 @@ def _get_datasets(g: Graph):
 def _get_dataset(g: Graph, dataset_iri):
     res = {"@id": dataset_iri,
            "@type": "Dataset",
-           "@created": time.time_ns(),
-           "@modified": time.time_ns(), }
+           "@created": str(time.time_ns()),
+           "@modified": str(time.time_ns()), }
 
     for _, p, o in g.triples((dataset_iri, None, None)):
+        obj = str(o)
         if p == dsp.hasTitle:
-            res['title'] = o
-        if p == dsp.hasConditionsOfAccess:
-            res['accessConditions'] = o
-        if p == dsp.hasHowToCite:
-            res['howToCite'] = o
-        if p == dsp.hasStatus:
-            res['status'] = o
+            res['title'] = obj
+        elif p == dsp.hasConditionsOfAccess:
+            res['accessConditions'] = obj
+        elif p == dsp.hasHowToCite:
+            res['howToCite'] = obj
+        elif p == dsp.hasStatus:
+            res['status'] = obj
+        elif p == dsp.hasAbstract:
+            res.setdefault("abstracts", {})
+            if isinstance(obj, BNode):
+                res['abstracts'].setdefault('urls', [])
+                res['abstracts'].get('urls').append(_get_url(g, o))
+            else:
+                res['abstracts'].setdefault('texts', [])
+                res['abstracts'].get('texts').append(_guess_language_of_text(obj))
+        elif p == dsp.hasTypeOfData:
+            res.setdefault("typeOfData", [])
+            res['typeOfData'].append(obj)
+        elif p == dsp.hasLicense:
+            res.setdefault("licenses", [])
+            res['licenses'].append(_get_url(g, o))
+        elif p == dsp.hasLanguage:
+            res.setdefault("languages", [])
+            res['languages'].append(_get_language_from_shortcode(obj))
+        elif p == dsp.hasQualifiedAttribution:
+            res.setdefault("attributions", [])
+            res['attributions'] = (_add_attribution(g, o, res['attributions']))
+        elif p == dsp.hasAlternativeTitle:
+            res.setdefault("alternativeTitles", [])
+            res['alternativeTitles'].append(_guess_language_of_text(obj))
+        elif p == dsp.hasDateCreated:
+            res['dateCreated'] = obj
+        elif p == dsp.hasDatePublished:
+            res['datePublished'] = obj
+        elif p == dsp.hasDateModified:
+            res['dateModified'] = obj
+        elif p == dsp.hasDistribution:
+            res['distribution'] = _get_url(g, o)
+        elif p == dsp.sameAs:
+            res.setdefault('urls', [])
+            res['urls'].append(_get_url(g, o))
+        elif p == dsp.hasDocumentation:
+            res.setdefault("documentations", {})
+            if isinstance(obj, BNode):
+                res['documentations'].setdefault('urls', [])
+                res['documentations'].get('urls').append(_get_url(g, o))
+            else:
+                res['documentations'].setdefault('texts', [])
+                res['documentations'].get('texts').append(_guess_language_of_text(obj))
+        elif p == dsp.isPartOf:
+            pass
+        # default cases
         elif p == RDF.type:
             pass
         else:
-            print("Issue: Could not handle:", p, o)
+            print("Issue: Could not handle in Dataset:", p, obj)
 
     return res
 
@@ -69,37 +117,38 @@ def _get_project(g: Graph):
     project_iri = next(g.triples((None, RDF.type, dsp.Project)))[0]
     res = {"@id": project_iri,
            "@type": "Project",
-           "@created": time.time_ns(),
-           "@modified": time.time_ns(),
+           "@created": str(time.time_ns()),
+           "@modified": str(time.time_ns()),
            "howToCite": "XXX",
            "datasets": []}
 
     for _, p, o in g.triples((project_iri, None, None)):
+        obj = str(o)
         if p == dsp.hasShortcode:
-            res['shortcode'] = o
+            res['shortcode'] = obj
         elif p == dsp.hasName:
-            res['name'] = o
+            res['name'] = obj
         elif p == dsp.hasDescription:
-            res['description'] = {"XXX": o}
+            res['description'] = {"XXX": obj}
         elif p == dsp.hasStartDate:
-            res['startDate'] = o
+            res['startDate'] = obj
         elif p == dsp.hasEndDate:
-            res['endDate'] = o
+            res['endDate'] = obj
         elif p == dsp.hasKeywords:
             res.setdefault('keywords', [])
-            res['keywords'].append(_guess_language(o))
+            res['keywords'].append(_guess_language_of_text(obj))
         elif p == dsp.hasDiscipline:
             res.setdefault('disciplines', [])
-            if isinstance(o, BNode):
+            if isinstance(obj, BNode):
                 res['disciplines'].append(_get_url(g, o))
             else:
-                res['disciplines'].append(_guess_language(o))
+                res['disciplines'].append(_guess_language_of_text(obj))
         elif p == dsp.hasTemporalCoverage:
             res.setdefault('temporalCoverage', [])
-            if isinstance(o, BNode):
+            if isinstance(obj, BNode):
                 res['temporalCoverage'].append(_get_url(g, o))
             else:
-                res['temporalCoverage'].append(_guess_language(o))
+                res['temporalCoverage'].append(_guess_language_of_text(obj))
         elif p == dsp.hasSpatialCoverage:
             res.setdefault('spatialCoverage', [])
             res['spatialCoverage'].append(_get_place(g, o))
@@ -107,30 +156,65 @@ def _get_project(g: Graph):
             res.setdefault('urls', [])
             res['urls'].append(_get_url(g, o))
         elif p == dsp.hasDataManagementPlan:
-            res['dataManagementPlan'] = o
+            res['dataManagementPlan'] = obj
         elif p == dsp.hasPublication:
             res.setdefault('publications', [])
-            res['publications'].append(o)
+            res['publications'].append(obj)
         elif p == dsp.hasGrant:
             res.setdefault('grants', [])
-            res['grants'].append(o)
+            res['grants'].append(obj)
         elif p == dsp.hasAlternateName:
             res.setdefault('alternativeNames', [])
-            res['alternativeNames'].append(_guess_language(o))
+            res['alternativeNames'].append(_guess_language_of_text(obj))
         elif p == dsp.hasFunder:
             res.setdefault('funders', [])
-            res['funders'].append(o)
+            res['funders'].append(obj)
         elif p == dsp.hasContactPoint:
-            res['contactPoint'] = o
+            res['contactPoint'] = obj
+        # default cases
         elif p == RDF.type:
             pass
         else:
-            print("Issue: Could not handle:", p, o)
+            print("Issue: Could not handle in Project:", p, obj)
 
     return res
 
 
-def _guess_language(text):
+# Utils
+# -----
+
+
+def _add_attribution(g: Graph, iri: BNode, attributions: List):
+    role = str(next(g.objects(iri, dsp.hasRole)))
+    agent = str(next(g.objects(iri, PROV.agent)))
+    for att in attributions:
+        p = att.get('person')
+        r = att.get('roles')
+        if r and p and p == agent:
+            r.append(role)
+            return attributions
+    attributions.append({"person": agent,
+                         "roles": [role]})
+    return attributions
+
+
+def _get_language_from_shortcode(code):
+    code = str(code)
+    langs = {'en': {'en': 'English',
+                    'de': 'Englisch',
+                    'fr': 'anglais'},
+             'de': {'en': 'German',
+                    'de': 'Deutsch',
+                    'fr': 'allemand'},
+             'fr': {'en': 'French',
+                    'de': 'Französisch',
+                    'fr': 'français'}}
+    default = {code: 'XXX - ' + code}
+    lang = langs.get(code)
+    return lang if lang else default
+
+
+def _guess_language_of_text(text):
     lang = detect(str(text))
     if lang not in ["en", "de", "fr"]:
         lang = f"XXX - {lang}"
@@ -148,9 +232,12 @@ def _get_place(g: Graph, iri: BNode):
 
 
 def _get_url(g: Graph, iri: BNode):
-    propID_bnode = next(g.objects(iri, SDO.propertyID))
-    propID = str(next(g.objects(propID_bnode, SDO.propertyID)))
     url = str(next(g.objects(iri, SDO.url)))
+    try:
+        propID_bnode = next(g.objects(iri, SDO.propertyID))
+        propID = str(next(g.objects(propID_bnode, SDO.propertyID)))
+    except StopIteration:
+        propID = url
     # LATER: improve text guessing
     return {
         "text": "XXX - " + propID,  # LATER: improve display text deduction
